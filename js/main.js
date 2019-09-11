c8f0f1b7d91ef94e422b3adf58227fbeaed2a098
amj311 <38047053+amj311@users.noreply.github.com>
2019-09-10 23:02:53 -0600

    hello wrold2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-function log(msg){
-	document.getElementById('msg').innerHTML += '<div>'+msg+'<br>';
+function log(msg, type){
+	const log = document.getElementById('msgs')
+	log.innerHTML += `<div class="msg ${type || ''}">
+											${msg} - 
+											<em>
+												${new Date().toISOString()}
+											</em></div>`;
+	log.scrollTop = log.scrollHeight;
 }
 
-log("hello world2")
-log("hello world2")
+
+
+
+/********************************************* */
+
+
+
+class DateTime {
+	constructor(initDate)
+	{
+		this.date()
+	}
+}
+
+
+
+/******************************************** */
+
+
+
+log(`Started app with ${userData.name}`, "err")
