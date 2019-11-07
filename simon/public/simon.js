9916f0472deeba96b8747ccf9f79dfbb65b17f11
amjudd315@gmail.com <amjudd315@gmail.com>
2019-11-07 09:15:35 +0000

    [rogress

--- simon/public/simon.js
+++ simon/public/simon.js
@@ -5,9 +5,10 @@ var game = new Vue ({
     el: '#game',
     
     data: {
-        loading: true,
-        GAME_STATES: ["loading","menu","playing","scores"],
-        gameState: null,
+        gameName: "Clement Says",
+        GAME_STATES: ["loading","login","menu","playing","lost","scores"],
+        gameState: "loading",
+        playerTurn: false,
         user: '',
         currentScore: 0,
         userScores: [],
@@ -17,13 +18,48 @@ var game = new Vue ({
         playingColors: [],
         simonColor: null,
         showSimonColor: false,
-        numColors: [],
+        numColors: 8,
+        uiColors: {play: null, scores: null, submit: null, back: null},
         allColors: ["blue","lime","purple","gold","red","darkorange","magenta", "aqua"]
     },
     
-    created() {
-        this.gameState = this.GAME_STATES[2]
-        axios.get('/scores?max=25').then( res => { console.log(res.data) } )
+    async created() {
+        this.gameState = this.GAME_STATES[3]
+        this.uiColors.play = this.randomFrom(this.allColors)
+        this.uiColors.scores = this.randomFrom(this.allColors)
+        this.uiColors.submit = this.randomFrom(this.allColors)
+        this.uiColors.back = this.randomFrom(this.allColors)
+        
+        let colorsLeft = Array.from(this.allColors)
+        for (let i = 0; i < this.numColors; i++) {
+            let idx = this.randomIdxOf(colorsLeft)
+            this.playingColors.push(this.allColors.indexOf(colorsLeft[idx]))
+            colorsLeft.splice(idx,1)
+            console.log(this.allColors[idx], colorsLeft)
+        }
+        
+        let scores = await axios.get('/scores?max=25')
+        console.log(scores.data)
         this.loading = false;
+    },
+    
+    methods: {
+        randomIdxOf(array){
+            return Math.round(Math.random()*(array.length-1))
+        },
+        
+        randomFrom(array){
+            return array[Math.round(Math.random()*(array.length-1))]
+        },
+        
+        startGame(){
+            
+        },
+        onUserAttempt(colorIdx){
+            this.showSimonColor = true
+            this.simonColor = this.allColors[colorIdx]
+        }
+        
+        
     },
 })
